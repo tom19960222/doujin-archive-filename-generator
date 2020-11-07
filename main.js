cc5a946d1330c191398a9357384a46c11cc46eaf
爆頭專家 <tom19960222@gmail.com>
2020-11-07 22:13:19 +0800

    Add files

---
+++ main.js
@@ -0,0 +1,69 @@
+
+import dayjs from 'dayjs'
+
+const prefixList = ['(同人ゲーム)', '(18禁ゲーム)', '(同人アニメ)', '(18禁アニメ)'];
+const prefixDropdownElementId = 'filename-generator-prefix-dropdown';
+const generatedFilenameTextboxId = 'filename-generator-generated-filename'
+let mainFrame, prefixDropdown, generatedFilenameTextbox;
+
+const extractDataFromPage = () => {
+    const makerName = document.querySelector('#work_maker > tbody > tr > td > span > a')?.textContent ?? '';
+    const workName = document.querySelector('#work_name > a')?.textContent ?? '';
+    const workId = document.URL.match(new RegExp('RJ[0-9]+.html'))[0]?.replace(/\.html/g, '') ?? '';
+
+    const saleDateStr = document.querySelector('#work_outline > tbody > tr:nth-child(1) > td > a')?.textContent ?? '';
+    const saleDate = saleDateStr === '' ? dayjs(saleDateStr.replace(/(年|月|日)/g, '-'), 'YYYY-MM-DD') : dayjs();
+
+    return {
+        makerName,
+        saleDate,
+        workName,
+        workId,
+    }
+}
+
+const createPrefixDropdown = () => {
+    const dropdown = document.createElement('select');
+    prefixList.forEach(prefix => {
+        const prefixOption = document.createElement('option');
+        prefixOption.innerText = prefix;
+        dropdown.appendChild(prefixOption);
+    });
+    dropdown.id = prefixDropdownElementId;
+    dropdown.value = prefixList?.[0] ?? '';
+    dropdown.onchange = updateGeneratedFilenameTextbox;
+    return dropdown;
+}
+
+const createGeneratedFilenameTextbox = () => {
+    const textbox = document.createElement('input');
+    textbox.type = 'text';
+    textbox.id = generatedFilenameTextboxId;
+    textbox.style.width = '700px';
+    textbox.style.height = 'auto';
+    return textbox;
+}
+
+const formatFilename = (data, prefix) => {
+    const formatedSaleDate = data.saleDate.format('YYMMDD');
+    return `${prefix} [${formatedSaleDate}][${data.workId}][${data.makerName}] ${data.workName}`
+}
+
+const updateGeneratedFilenameTextbox = () => {
+    generatedFilenameTextbox.value = formatFilename(extractDataFromPage(), prefixDropdown.value);
+}
+
+const init = () => {
+    mainFrame = document.createElement('div');
+    prefixDropdown = createPrefixDropdown();
+    generatedFilenameTextbox = createGeneratedFilenameTextbox();
+    
+    mainFrame.appendChild(prefixDropdown);
+    mainFrame.appendChild(generatedFilenameTextbox);
+    updateGeneratedFilenameTextbox();
+    
+    document.querySelector('#top_wrapper').appendChild(mainFrame);
+}
+
+if(document.URL.match(/maniax\/work\/=\/product_id\/RJ[0-9]+.html/))
+  init();
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Doujin Game Archive Filename Generator
+
+A simple tool to generate filename when making game archive.
+
+## Getting Started
+
+1. Clone this repo
+2. Run `yarn` to install dependencies
+3. Load extension through Chrome
+4. Open any DLSite doujin game page (currently only Japanese page supported.)
+Example here: [NSFW Warning!](https://www.dlsite.com/maniax/work/=/product_id/RJ162718.html)
+
+## Screenshot
